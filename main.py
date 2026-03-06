ce4471aac91d13f38802d6a2b7ef333b867324da
Guillermo leaño <34119019+guillermoleano@users.noreply.github.com>
2026-03-06 17:25:31 -0500

    Clean up main.py by removing unused code
    Removed commented-out code and unused functions.

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-'''
-#configuracion inicial del sistema ✅
-saldo_inicial = 1000 ✅
-pin = 1234✅
-intentos_pin = 3 ✅
-funcion_de_bienvenida() ❌
-autenticacion() # se debe incluir control de intentos    ✅
-menu_operaciones()  #consultar saldo , retirar dinero , depositar dinero , salir❌
-consultar() ❌
-retirar()❌
-depositar()❌
-salir()❌
-funcion_de_despedida()❌
-'''
 
 from utils import authentication
 # configuracion inicial

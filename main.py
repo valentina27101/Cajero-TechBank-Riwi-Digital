ae5e865f96a9df63667222cd8708b666b458a982
Guillermo leaño <34119019+guillermoleano@users.noreply.github.com>
2026-03-06 17:30:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-'''
-#configuracion inicial del sistema ✅
-saldo_inicial = 1000 ✅
-pin = 1234✅
-intentos_pin = 3 ✅
-funcion_de_bienvenida() ❌
-autenticacion() # se debe incluir control de intentos    ✅
-menu_operaciones()  #consultar saldo , retirar dinero , depositar dinero , salir❌
-consultar() ❌
-retirar()❌
-depositar()❌
-salir()❌
-funcion_de_despedida()❌
-'''
+

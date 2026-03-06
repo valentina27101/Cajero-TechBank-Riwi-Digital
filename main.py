ed9c1e586364a4c8950405514b656167246f3327
Guillermo leaño <34119019+guillermoleano@users.noreply.github.com>
2026-03-06 13:27:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -15,38 +15,38 @@ funcion_de_despedida()
 
 # Saldo Inicial Del Cajero
 Saldo = 1000
-pin = 1234
-intentos_pin = 3 
+pin = "1234"
 
 # Autenticación
 def Autenticacion():
- intentos_pin = 3 
+    intentos_pin = 3 
 
-while intentos_pin > 0:
-    pin_usuario = input("ingrese su PIN: ")
+    while intentos_pin > 0:
+        pin_usuario = input("Ingrese su PIN: ")
     
-    # Validar Que Solo Tenga Numeros
-    if not pin_usuario.isdigit():
-       print("Error: el PIN solo debe tener numeros.")
-       intentos_pin -= 1
-       print("Intentos restantes: ", intentos_pin)
-       continue
+        # Validar que solo tenga números
+        if not pin_usuario.isdigit():
+            print("Error: el PIN solo debe tener numeros.")
+            intentos_pin -= 1
+            print("Intentos restantes:", intentos_pin)
+            continue
 
-    # Validar Que Tenga 4 Digitos 
-    if len(pin_usuario) != 4:
-       print("Error: el PIN debe tener 4 digitos.")
-       intentos_pin -= 1
-       print("Intentos restantes: ", intentos_pin)
-       continue
+        # Validar que tenga 4 dígitos
+        if len(pin_usuario) != 4:
+            print("Error: el PIN debe tener 4 digitos.")
+            intentos_pin -= 1
+            print("Intentos restantes:", intentos_pin)
+            continue
     
-    if pin_usuario == pin:
-       print("Acceso permitido: ")
-       print("Su Saldo es: ", Saldo)
-       break
-    else: 
-       intentos_pin -= 1
-       print("PIN incorrecto.")
-       print("Intentos restantes: ", intentos_pin)
+        if pin_usuario == pin:
+            print("Acceso permitido")
+            break
+        else: 
+            intentos_pin -= 1
+            print("PIN incorrecto")
+            print("Intentos restantes:", intentos_pin)
 
-if intentos_pin == 0:
-    print("Cuenta Bloqueada.")
+    if intentos_pin == 0:
+        print("Cuenta bloqueada")
+
+Autenticacion()

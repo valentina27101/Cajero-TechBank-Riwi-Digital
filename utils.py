951f96e490979d8e2650d6adbf9d5626bd6aabfc
Guillermo leaño <34119019+guillermoleano@users.noreply.github.com>
2026-03-06 13:29:21 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,30 @@
+def autenticacion():
+    intentos_pin = 3 
 
+    while intentos_pin > 0:
+        pin_usuario = input("Ingrese su PIN: ")
+    
+        # Validar que solo tenga números
+        if not pin_usuario.isdigit():
+            print("Error: el PIN solo debe tener numeros.")
+            intentos_pin -= 1
+            print("Intentos restantes:", intentos_pin)
+            continue
+
+        # Validar que tenga 4 dígitos
+        if len(pin_usuario) != 4:
+            print("Error: el PIN debe tener 4 digitos.")
+            intentos_pin -= 1
+            print("Intentos restantes:", intentos_pin)
+            continue
+    
+        if pin_usuario == pin:
+            print("Acceso permitido")
+            break
+        else: 
+            intentos_pin -= 1
+            print("PIN incorrecto")
+            print("Intentos restantes:", intentos_pin)
+
+    if intentos_pin == 0:
+        print("Cuenta bloqueada")

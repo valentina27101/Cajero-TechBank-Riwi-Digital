0b4d1968875e619dfaf7da260cbc4fafe37e5db1
mateoriwi <sharonmilena.mmm@gmail.com>
2026-03-06 16:38:09 -0500

    Se agrega el main

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-'''
-#configuracion inicial del sistema ✅
-saldo_inicial = 1000 ✅
-pin = 1234✅
-intentos_pin = 3 ✅
-funcion_de_bienvenida() ❌
-autenticacion() # se debe incluir control de intentos    ✅
-menu_operaciones()  #consultar saldo , retirar dinero , depositar dinero , salir❌
-consultar() ❌
-retirar()❌
-depositar()❌
-salir()❌
-funcion_de_despedida()❌
-'''
+from colores import *
+from menuuu import *
+
+
+#Aquí va el autenticador#
+
+
+color_pagina_principal("Iniciaste sesión correctamente, bienvenid@ al cajero TechBank de Riwi")
+    
+saldo_inicial = 1000
+
+menu_cajero()

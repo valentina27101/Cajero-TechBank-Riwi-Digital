717beb2986566aacaf611ab037645d1d338974f9
Guillermo leaño <34119019+guillermoleano@users.noreply.github.com>
2026-03-09 13:19:46 -0500

    Update main.py
    main actualizado

--- main.py
+++ main.py
@@ -5,23 +5,12 @@ import time
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
-
-Saldo = 1000
-
-
 clear()
 color_pagina_principal("Bienvenid@ al cajero TechBank de Riwi")
-
+time.sleep(2)  
 
 authentication(pin="1234", intentos_pin=3)
-
-
 clear()
-
-
 time.sleep(1)  
-
-saldo_inicial = 1000
-
 
 menu_cajero()
